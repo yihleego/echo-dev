92a2ed5b5c050027d667f5750ff241bb1df4eb18
Leego <i@leego.io>
2024-01-05 16:04:21 +0800

    Copyright

--- echo-core/echo/utils/__init__.py
+++ echo-core/echo/utils/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024  Echo Authors. All Rights Reserved.
+# Copyright (c) 2024 Echo Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 from .common import to_string, deep_to_lower, deep_to_upper, deep_strip, \
     matches, STR_EXPRS, INT_EXPRS, BOOL_EXPRS
