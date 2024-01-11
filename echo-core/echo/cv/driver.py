d3b0fe2a6b7f0ba1be6479ba1d1d761c2dd44ade
Leego <i@leego.io>
2024-01-11 13:46:19 +0800

    111

--- echo-core/echo/cv/driver.py
+++ echo-core/echo/cv/driver.py
@@ -17,15 +17,46 @@
 from typing import Optional
 
 from echo.driver import Driver, Element
+from echo.utils import win32, to_string
 
 
 class CVElement(Element):
-    pass
+    def __init__(self, handle: int, process_id: int, process_name: str, root: 'CVElement' = None, parent: 'CVElement' = None):
+        self._handle: int = handle
+        self._process_id: int = process_id
+        self._process_name: str = process_name
+        self._root: CVElement = root or self  # TODO root
+        self._parent: Optional[CVElement] = parent
+
+    @property
+    def handle(self) -> int:
+        return self._handle
+
+    @property
+    def process_id(self) -> int:
+        return self._process_id
+
+    @property
+    def process_name(self) -> str:
+        return self._process_name
+
+    def __str__(self) -> str:
+        return to_string(self, 'handle', 'process_id', 'process_name')
+
+    @staticmethod
+    def create_root(handle: int) -> Optional['CVElement']:
+        process_id = win32.get_process_id_from_handle(handle)
+        process_name = win32.get_process_name_by_process_id(process_id)
+        return CVElement(handle=handle, process_id=process_id, process_name=process_name)
+
+    @staticmethod
+    def create_element(root: 'CVElement', parent: 'CVElement' = None) -> 'CVElement':
+        return CVElement(handle=root._handle, process_id=root._process_id, process_name=root._process_name, root=root, parent=parent)
 
 
 class CVDriver(Driver):
     def find_window(self, handle: int) -> Optional[CVElement]:
-        return None
+        return CVElement.create_root(handle=handle)
 
     def close(self):
         pass
