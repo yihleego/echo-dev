d87638794234345e5987c0ffc60b6171b46b8d38
Leego <i@leego.io>
2024-01-12 12:13:21 +0800

    aaaaa

---
+++ echo-core/echo/cv/matching/matching.py
@@ -0,0 +1,171 @@
+from abc import ABC, abstractmethod
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from echo.cv.aircv import TemplateInputError
+
+
+class Matching(ABC):
+    def __init__(self, im_search, im_source, threshold: float = 0.8, rgb: bool = True):
+        super().__init__()
+        self.im_source = im_source
+        self.im_search = im_search
+        self.threshold: float = threshold
+        self.rgb: bool = rgb
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_all_results(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_best_result(self):
+        raise NotImplementedError
+
+
+def generate_result(middle_point, pypts, confi):
+    """Format the result: 定义图像识别结果格式."""
+    ret = dict(result=middle_point,
+               rectangle=pypts,
+               confidence=confi)
+    return ret
+
+
+def check_image_valid(im_source, im_search):
+    """Check if the input images valid or not."""
+    if im_source is not None and im_source.any() and im_search is not None and im_search.any():
+        return True
+    else:
+        return False
+
+
+def check_source_larger_than_search(im_source, im_search):
+    """检查图像识别的输入."""
+    # 图像格式, 确保输入图像为指定的矩阵格式:
+    # 图像大小, 检查截图宽、高是否大于了截屏的宽、高:
+    h_search, w_search = im_search.shape[:2]
+    h_source, w_source = im_source.shape[:2]
+    if h_search > h_source or w_search > w_source:
+        raise TemplateInputError("error: in template match, found im_search bigger than im_source.")
+
+
+def img_mat_rgb_2_gray(img_mat):
+    """
+    Turn img_mat into gray_scale, so that template match can figure the img data.
+    "print(type(im_search[0][0])")  can check the pixel type.
+    """
+    assert isinstance(img_mat[0][0], np.ndarray), "input must be instance of np.ndarray"
+    return cv2.cvtColor(img_mat, cv2.COLOR_BGR2GRAY)
+
+
+def pil_2_cv2(pil_image):
+    open_cv_image = np.array(pil_image)
+    # Convert RGB to BGR (method-1):
+    open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
+    # Convert RGB to BGR (method-2):
+    # b, g, r = cv2.split(open_cv_image)
+    # open_cv_image = cv2.merge([r, g, b])
+    return open_cv_image
+
+
+def cv2_2_pil(cv2_image):
+    cv2_im = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
+    pil_im = Image.fromarray(cv2_im)
+    return pil_im
+
+
+def compress_image(pil_img, path, quality, max_size=None):
+    """
+    Save the picture and compress
+
+    :param pil_img: PIL image
+    :param path: save path
+    :param quality: the image quality, integer in range [1, 99]
+    :param max_size: the maximum size of the picture, e.g 1200
+    :return:
+    """
+    if max_size:
+        # The picture will be saved in a size <= max_size*max_size
+        pil_img.thumbnail((max_size, max_size), Image.LANCZOS)
+    quality = int(round(quality))
+    if quality <= 0 or quality >= 100:
+        raise Exception("SNAPSHOT_QUALITY (" + str(quality) + ") should be an integer in the range [1,99]")
+    pil_img.save(path, quality=quality, optimize=True)
+
+
+def cal_ccoeff_confidence(im_source, im_search):
+    """求取两张图片的可信度，使用TM_CCOEFF_NORMED方法."""
+    # 扩展置信度计算区域
+    im_source = cv2.copyMakeBorder(im_source, 10, 10, 10, 10, cv2.BORDER_REPLICATE)
+    # 加入取值范围干扰，防止算法过于放大微小差异
+    im_source[0, 0] = 0
+    im_source[0, 1] = 255
+
+    im_source, im_search = img_mat_rgb_2_gray(im_source), img_mat_rgb_2_gray(im_search)
+    res = cv2.matchTemplate(im_source, im_search, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    return max_val
+
+
+def cal_rgb_confidence(img_src_rgb, img_sch_rgb):
+    """同大小彩图计算相似度."""
+    # 减少极限值对hsv角度计算的影响
+    img_src_rgb = np.clip(img_src_rgb, 10, 245)
+    img_sch_rgb = np.clip(img_sch_rgb, 10, 245)
+    # 转HSV强化颜色的影响
+    img_src_rgb = cv2.cvtColor(img_src_rgb, cv2.COLOR_BGR2HSV)
+    img_sch_rgb = cv2.cvtColor(img_sch_rgb, cv2.COLOR_BGR2HSV)
+
+    # 扩展置信度计算区域
+    img_src_rgb = cv2.copyMakeBorder(img_src_rgb, 10, 10, 10, 10, cv2.BORDER_REPLICATE)
+    # 加入取值范围干扰，防止算法过于放大微小差异
+    img_src_rgb[0, 0] = 0
+    img_src_rgb[0, 1] = 255
+
+    # 计算BGR三通道的confidence，存入bgr_confidence
+    src_bgr, sch_bgr = cv2.split(img_src_rgb), cv2.split(img_sch_rgb)
+    bgr_confidence = [0, 0, 0]
+    for i in range(3):
+        res_temp = cv2.matchTemplate(src_bgr[i], sch_bgr[i], cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res_temp)
+        bgr_confidence[i] = max_val
+
+    return min(bgr_confidence)
+
+
+def crop_image(img, rect):
+    """
+        区域截图，同时返回截取结果 和 截取偏移;
+        Crop image , rect = [x_min, y_min, x_max ,y_max].
+        (airtest中有用到)
+    """
+
+    if isinstance(rect, (list, tuple)) and len(rect) == 4:
+        height, width = img.shape[:2]
+        # 获取在图像中的实际有效区域：
+        x_min, y_min, x_max, y_max = [int(i) for i in rect]
+        x_min, y_min = max(0, x_min), max(0, y_min)
+        x_min, y_min = min(width - 1, x_min), min(height - 1, y_min)
+        x_max, y_max = max(0, x_max), max(0, y_max)
+        x_max, y_max = min(width - 1, x_max), min(height - 1, y_max)
+
+        # 返回剪切的有效图像+左上角的偏移坐标：
+        img_crop = img[y_min:y_max, x_min:x_max]
+        return img_crop
+    else:
+        raise Exception("to crop a image, rect should be a list like: [x_min, y_min, x_max, y_max].")
+
+
+def check_cv_version_is_new():
+    """opencv版本是3.0或4.0以上, API接口与2.0的不同."""
+    if cv2.__version__.startswith("3.") or cv2.__version__.startswith("4."):
+        return True
+    else:
+        return False
